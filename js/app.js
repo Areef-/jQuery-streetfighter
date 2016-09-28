ee5f63bc29087b54fd673db718b50df3a051d8d6
Areef Ahmed <areef.ahmed92@gmail.com>
2016-09-29 01:08:27 +0600

    all animation done excluding x

--- js/app.js
+++ js/app.js
@@ -8,16 +8,34 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
-		
+		playHadouken ();
 		//play hadouken sound
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
-		//animate it to the right	 of the screen
+		$(".hadouken").finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(".hadouken").hide();
+				$(".hadouken").css('left', '520px');
+			}
+		);
 	})
 	.mouseup(function() {
 		//ryu goes back to ready position
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
 	});
+	playBgMusic();
 });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.4;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+function playBgMusic () {
+  $('#bg-music')[0].volume = 0.1;
+  $('#bg-music')[0].load();
+  $('#bg-music')[0].play();
+}
